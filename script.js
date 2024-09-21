3f1dc3305d01989fc05e0cc407b75aa14cb69021
gurainzu <gurainzu@github.com>
2024-09-21 14:31:47 +0700

    Prettier the code

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
 // Back to Top Function
 const mybutton = document.getElementById("backtotop");
+const github = document.getElementById("github");
 
 const scrollFunction = () => {
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      mybutton.classList.remove("hidden");
-    } else {
-      mybutton.classList.add("hidden");
-    }
-  };
-  const backToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
-  
-  // When the user clicks on the button, scroll to the top of the document
-  mybutton.addEventListener("click", backToTop);
-  
-  window.addEventListener("scroll", scrollFunction);
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.classList.remove("hidden");
+    github.classList.remove("hidden");
+  } else {
+    mybutton.classList.add("hidden");
+    github.classList.add("hidden");
+  }
+};
+const backToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
 
-  // Drag scrolling function
-    const slider = document.querySelector('.animelist');
-    let isDown = false;
-    let startX;
-    let scrollLeft;
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
 
-    slider.addEventListener('mousedown', (e) => {
-        isDown = true;
-        slider.classList.add('active');
-        startX = e.pageX - slider.offsetLeft;
-        scrollLeft = slider.scrollLeft;
-    });
+window.addEventListener("scroll", scrollFunction);
 
-    slider.addEventListener('mouseleave', () => {
-        isDown = false;
-        slider.classList.remove('active');
-    });
+// Drag scrolling function
+const slider = document.querySelector(".animelist");
+let isDown = false;
+let startX;
+let scrollLeft;
 
-    slider.addEventListener('mouseup', () => {
-        isDown = false;
-        slider.classList.remove('active');
-    });
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
 
-    slider.addEventListener('mousemove', (e) => {
-        if (!isDown) return;
-        e.preventDefault();
-        const x = e.pageX - slider.offsetLeft;
-        const walk = (x - startX) * 2; // Adjust scrolling speed
-        slider.scrollLeft = scrollLeft - walk;
-    });
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2; // Adjust scrolling speed
+  slider.scrollLeft = scrollLeft - walk;
+});
